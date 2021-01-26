ac8a808e4a35c705559b7f43d5fd40df5e4bbebf
iamrivera <a3m3rivera@gmail.com>
2021-01-26 00:13:19 -0800

    Adding the seed file.

---
+++ db/migrate/seeds.rb
@@ -0,0 +1,4 @@
+#Add seed data here. Remember to seed database with rake db:seed
+anita = Doctor.create(first_name: "Anita", last_name: "Kahlo", email: "akahlo@docpoc.com", bio: "I am a doctor! See me cure!", specialty: "Musical Medicine", medical_school: "USC - Keck School of Medicine", residency: "USC - LA County", graduation_date: "1991", password: "seedbead19")
+Patient.create(first_name: "Barry", last_name: "Sanders", email: "barrys@anders.com", password: "GoSoxs!", occupation: "Athlete", marital_status: "Divorced", birthday: "1999-01-03", doctor: anita)
+Patient.create(first_name: "Larry", last_name: "Bulls", email: "larryb@ulls.com", password: "GoBulls!", occupation: "Athlete", marital_status: "Divorced", birthday: "1999-04-03", doctor: anita)
