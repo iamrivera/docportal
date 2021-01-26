cbde73c56fbee5947a9e56399e94bc1f65555ca3
iamrivera <a3m3rivera@gmail.com>
2021-01-26 01:33:15 -0800

    Built the doctor registration page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,14 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  helpers do 
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      user.find_by(session[:user_id])
+    end
+  end
+
 end
